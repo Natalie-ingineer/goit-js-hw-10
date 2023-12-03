62b68f3a281a609cbbf80d52354375bf02e55683
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-03 20:56:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const div = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const errorMessage = document.querySelector('.error');
 
+loader.addEventListener('click', onLoaderVisible);
+
+loader.style.display = 'none';
+errorMessage.style.display = 'none';
+
 fetchBreeds()
   .then(data => select.insertAdjacentHTML('beforeend', createMarkup(data)))
   .catch(err => console.log(err));
@@ -18,12 +23,8 @@ function createMarkup(arr) {
     .join('');
 }
 
-// loader.addEventListener('click', onLoader);
-
-// loader.style.display = 'none';
-// errorMessage.style.display = 'none';
-
 select.addEventListener('change', () => {
+  onLoaderVisible();
   const selectedCat = select.value;
   fetchCatByBreed(selectedCat)
     .then(info => div.insertAdjacentHTML('beforeend', createMarkupCat(info)))
@@ -35,17 +36,25 @@ function createMarkupCat(data) {
 
   const { name, description, temperament } = cat;
 
+  onLoaderHidden();
   return `
-    <img src="${data[0].url}" alt="${name}" weight="350">
-    <h2>${name}</h2>
-    <p>${description}</p>
-    <p>${temperament}</p>
+  <img src="${data[0].url}" alt="${name}" weight="350">
+  <h2>${name}</h2>
+  <p>${description}</p>
+  <p>${temperament}</p>
   `;
 }
 
-// function onLoader() {
-//   currentPage += 1;
-//   fetchBreeds(currentPage)
-//     .then(data => select.insertAdjacentHTML('beforeend', createMarkup(data)))
-//     .catch(err => console.log(err));
-// }
+function onLoaderVisible() {
+  select.style.display = 'none';
+  loader.style.display = 'block';
+  div.style.display = 'none';
+}
+
+console.log('hello');
+
+function onLoaderHidden() {
+  loader.style.display = 'none';
+  select.style.display = 'block';
+  div.style.display = 'block';
+}
