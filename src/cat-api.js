0afc3b45fc3bbc67f54fd4ce7c2cddc8879fd5f6
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-04 19:51:24 +0200

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -9,51 +9,6 @@ import '/node_modules/slim-select/dist/slimselect.min.js';
 axios.defaults.headers.common['x-api-key'] =
   'live_QnYFBW48Rw6I7bGfsFA1QTbYdWtKQKT86j8h8KpF4TCTSz8rp4W4DFTADxItBVig';
 
-import Notiflix from 'notiflix';
-
-import Notiflix from 'notiflix/dist/notiflix-aio-3.2.6.min.js';
-
-Notiflix.Notify.init({
-  width: '280px',
-  position: 'right-top', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
-  distance: '10px',
-  opacity: 1,
-  borderRadius: '5px',
-  rtl: false,
-  timeout: 3000,
-  messageMaxLength: 110,
-  backOverlay: false,
-  backOverlayColor: 'rgba(0,0,0,0.5)',
-  plainText: true,
-  showOnlyTheLastOne: false,
-  clickToClose: false,
-  pauseOnHover: true,
-
-  ID: 'NotiflixNotify',
-  className: 'notiflix-notify',
-  zindex: 4001,
-  fontFamily: 'Quicksand',
-  fontSize: '13px',
-  cssAnimation: true,
-  cssAnimationDuration: 400,
-  cssAnimationStyle: 'fade', // 'fade' - 'zoom' - 'from-right' - 'from-top' - 'from-bottom' - 'from-left'
-  closeButton: false,
-  useIcon: true,
-  useFontAwesome: false,
-  fontAwesomeIconStyle: 'basic', // 'basic' - 'shadow'
-  fontAwesomeIconSize: '34px',
-
-  failure: {
-    background: '#ff5549',
-    textColor: '#fff',
-    childClassName: 'notiflix-notify-failure',
-    notiflixIconColor: 'rgba(0,0,0,0.2)',
-    fontAwesomeClassName: 'fas fa-times-circle',
-    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
-    backOverlayColor: 'rgba(255,85,73,0.2)',
-  },
-});
-
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 const END_POINTS = 'breeds';
 
